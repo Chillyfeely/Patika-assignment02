ea2d70a2bc9e6c7512b48e1774d31808bc4fe2be
Chillyfeely <vayselcacan@gmail.com>
2023-04-11 04:34:42 +0300

    second commit after changes in assignment

--- counter.sol
+++ counter.sol
@@ -2,15 +2,24 @@
 pragma solidity ^0.8.4;
 
 contract TestCounter {
-    int private count = 0;
-    function incrementCounter() public {
-        count += 1;
-    }
-    function decrementCounter() public {
-        count -= 1;
-    }
+    int private total = 0;
+    int private substracted = 0;
+    int private multiplied = 0;
+    int private divided = 0;
 
-    function getCount() public view returns (int) {
-        return count;
+    function add(int a, int b) public {
+        total = a + b;
+    }
+    function substract(int a, int b) public{
+       substracted = a - b;
+    }
+    function multiply(int a, int b) public {
+        multiplied = a * b;
+    }
+    function divide(int a, int b) public {
+        divided = a / b;
+    }
+    function multiReturn() public view returns (int, int, int, int){
+        return (total, substracted, multiplied, divided);
     }
 }
